aeb3055c470dfc8e02205f8d618e7e3b3744203b
SebastianMattsson <sebbe.mattsson.94@gmail.com>
2024-01-30 08:48:19 +0100

    finsished most of the portfolio, just have to expand the projects

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ function showSlides() {
   slides[slideIndex - 1].style.opacity = 1;
   setTimeout(showSlides, 5000); // Change image every 2 seconds
 }
+
+function opentab(tabName) {
+  // Hide all elements with class="tab-contents" by default
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tab-contents");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+
+  // Remove the class "active-link" from all elements
+  tablinks = document.getElementsByClassName("tab-links");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active-link", "");
+  }
+
+  // Show the specific tab content and add an "active-link" class to the clicked tab
+  document.getElementById(tabName).style.display = "block";
+  event.currentTarget.className += " active-link";
+}
+
+function openmenu() {
+  document.getElementById("sidemenu").style.right = "0";
+}
+
+function closemenu() {
+  document.getElementById("sidemenu").style.right = "-200px"; // Assuming -200px is the initial position of the sidemenu.
+}
